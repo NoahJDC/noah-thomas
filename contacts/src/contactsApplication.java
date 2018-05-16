2b6639da7187989e5bc8895bee1c9bf5578e70e3
Noah Dela Cruz <noahjdc92@gmai.com>
2018-05-16 12:35:37 -0500

    added bonus

--- contacts/src/contactsApplication.java
+++ contacts/src/contactsApplication.java
@@ -1,13 +1,10 @@
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-
 
 
 public class contactsApplication {
@@ -64,14 +61,25 @@ public class contactsApplication {
     }
 
     public static void importFile(String file, String directory){
-        Path directoryName = Paths.get(directory);
-        Path fileName = Paths.get(directory, file);
+        contactList.clear();
         try {
             List<String> info = Files.readAllLines(Paths.get(directory, file));
+            String number = "";
+            String name = "";
+
             for (String line: info){
                 String[] divide = line.split(" ");
-                String name = divide[0] + " " + divide[1];
-                String number = divide[2];
+                name = divide[0] + " " + divide[1];
+                number = divide[2];
+                String[] numberArray = divide[2].split("");
+
+                if(numberArray.length == 10){
+                    number = tenDigits(numberArray);
+
+                } else if(numberArray.length == 7){
+                    number = sevenDigits(numberArray);
+                }
+
                 contacts newContact = new contacts(name, number);
                 contactList.add(newContact);
             }
@@ -82,11 +90,76 @@ public class contactsApplication {
         }
     }
 
+    public static String sevenDigits(String [] list) {
+        String number = "";
+        String[] firstHalf = new String[4];
+        String[] secondHalf = new String[4];
+        for(int i = 0; i < 3; i++){
+            firstHalf[i] = list[i];
+        }
+        firstHalf[3] = "-";
+        for(int i = 0; i < 4; i++){
+            secondHalf[i] = list[i+3];
+        }
+
+        for(int i = 0; i < 4; i++){
+            number += firstHalf[i];
+        }
+        for(int i = 0; i < 4; i++){
+            number += secondHalf[i];
+        }
+        return number;
+    }
+
+    public static String tenDigits(String [] list){
+        String number = "";
+        String[] firstHalf = new String[4];
+        String[] secondHalf = new String[4];
+        String[] thirdHalf = new String[4];
+        int i;
+
+        for(i = 0; i < 3; i++){
+            firstHalf[i] = list[i];
+        }
+        firstHalf[3] = "-";
+        for(i = 0; i<4; i++){
+            secondHalf[i] = list[i+2];
+        }
+        secondHalf[3]="-";
+        for(i=0;i<4; i++){
+            thirdHalf[i] = list[i+3];
+        }
+        for(i = 0; i<4; i++){
+            number += firstHalf[i];
+        }
+        for(i = 0; i<4; i++){
+            number += secondHalf[i];
+        }
+        for(i = 0;i<4;i++){
+            number += thirdHalf[i];
+        }
+        return number;
+    }
+
+
+
     public static void addContact(String file, String directory){
         System.out.println("Enter a contact name: ");
         String name = Input.getString();
+
+        if(checkName(name) == true){
+            System.out.println("That contact already exists!");
+            addContact("contactList.txt", "data");
+        }
         System.out.println("Enter their number: ");
         String number = Input.getString();
+        if(number.contains("-")){
+            System.out.println("Please enter a number with only integers!");
+            addContact("contactList.txt", "data");
+        } else if(number.length() < 7 || number.length() > 10 || number.length() == 9 || number.length() == 8 ){
+            System.out.println("That is not an acceptable length!");
+            addContact("contactList.txt", "data");
+        }
         contacts thisPunk = new contacts(name, number);
         contactList.add(thisPunk);
         String contact = name + " " + number;
@@ -99,16 +172,37 @@ public class contactsApplication {
         catch(IOException e){
             System.out.println(e.getMessage());
         }
+        System.out.println();
+        updateInfo("contactList.txt", "data");
+        importFile("contactList.txt", "data");
         continueProgram();
+
+    }
+
+    public static Boolean checkName(String name){
+        Boolean check = false;
+        for(contacts index: contactList){
+            if(index.getName().equalsIgnoreCase(name)){
+                check = true;
+            }
+
+        }
+        return check;
 
     }
 
     public static void viewContacts(){
+        String headerName = String.format("%-20s|", "Name");
+        System.out.println(headerName + "  Phone Number");
+        System.out.println("-----------------------------------");
         for(contacts index: contactList){
-            System.out.println(index.getName() + " " + index.getNumber());
+            String output = String.format("%-20s|  ",index.getName());
+            System.out.println(output + index.getNumber());
         }
+        System.out.println();
         continueProgram();
     }
+
 
     public static void searchContacts(){
         System.out.println("Enter the name you are looking for");
@@ -120,13 +214,14 @@ public class contactsApplication {
                 trueName = 1;
             } else if(index.getName().contains(name)){
                 System.out.println("Is this the one you were looking for?");
-                System.out.println(index.getName() + " " + index.getNumber());
+                System.out.println(index.getName() + ": " + index.getNumber());
                 trueName = 1;
             }
         }
         if(trueName == 0){
             System.out.println("No one has that name!");
         }
+        System.out.println();
         continueProgram();
 
     }
@@ -140,6 +235,7 @@ public class contactsApplication {
                 break;
             }
         }
+        System.out.println();
         continueProgram();
     }
 
